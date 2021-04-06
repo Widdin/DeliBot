78cb528f08fe66f6e2a50a3a53ee76c6afb14975
widdin <widdimon@hotmail.com>
2021-04-06 16:12:12 +0200

    Exit & log if no config is found

--- delibot/utils/default.py
+++ delibot/utils/default.py
@@ -1,7 +1,16 @@
 import configparser
+import logging
+
+log = logging.getLogger()
 
 
 def get_config():
     config = configparser.ConfigParser()
-    config.read('config/config.ini')
+
+    try:
+        config.read_file(open('config/config.ini', 'r'))
+    except FileNotFoundError as e:
+        log.critical(f'Could not find the configuration file. Reason: {e}')
+        exit()
+
     return config
