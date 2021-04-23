997fc60002bd9536bb9c7f4b825b993258b85715
widdin <widdimon@hotmail.com>
2021-04-23 16:35:54 +0200

    Community day: Set to NULL if message doesn't exist

--- delibot/cogs/admin.py
+++ delibot/cogs/admin.py
@@ -129,7 +129,9 @@ class Admin(commands.Cog):
                 try:
                     await self.bot.http.edit_message(int(channel_id), int(message_id), embed=embed.to_dict())
                 except discord.errors.NotFound:
-                    pass
+                    query = "UPDATE settings SET community_day_channel_id = NULL, community_day_message_id = NULL WHERE server_id = %s"
+                    params = (server_id, )
+                    await self.bot.db.execute(query, params)
                 except discord.errors.Forbidden:
                     pass
 
