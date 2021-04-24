bae7510bc0c04eac08102a040528daa18dbcbb55
widdin <widdimon@hotmail.com>
2021-04-24 20:50:13 +0200

    Raid overview: Set to NULL if message doesn't exist

--- delibot/cogs/admin.py
+++ delibot/cogs/admin.py
@@ -836,7 +836,9 @@ class Admin(commands.Cog):
                 try:
                     await self.bot.http.edit_message(channel_id, message_id, embed=embed.to_dict())
                 except discord.errors.NotFound:
-                    pass
+                    query = "UPDATE settings SET raid_overview_channel_id = NULL, raid_overview_message_id = NULL WHERE server_id = %s"
+                    params = (server_id,)
+                    await self.bot.db.execute(query, params)
                 except discord.errors.Forbidden:
                     pass
                 except discord.errors.HTTPException:
